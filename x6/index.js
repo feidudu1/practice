cdbdfb266ff7ae38c43eea84c5d103ae9286ef56
youngdudu <342363079@qq.com>
2023-07-25 10:54:08 +0800

    fix

---
+++ x6/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var x6_1 = require("@antv/x6");
+var graph = new x6_1.Graph({
+    container: document.getElementById('container'),
+    width: 800,
+    height: 600,
+    background: {
+        color: '#F2F7FA',
+    },
+});
